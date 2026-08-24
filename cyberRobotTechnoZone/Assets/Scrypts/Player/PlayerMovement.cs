7059ba9b44ee90f99dbf9a16d3e000b6a86f4205
The-seagull-man <mail@marenvalentin.dk>
2026-08-24 23:21:01 +0200

    walking men jump er fucked med colliders nogle gange
    Jeg fikser det i morgen når jeg ik har lyst til at dø så meget <3

--- cyberRobotTechnoZone/Assets/Scrypts/Player/PlayerMovement.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/Player/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     private float jumpCooldown = 1f;
     [SerializeField] private float currentCooldownTime;
 
+    ContactPoint2D contactPoint;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,25 +29,47 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
-        if (Keyboard.current.wKey.isPressed && grounded == true && currentCooldownTime <= 0)
+        if ((Keyboard.current.wKey.isPressed || Keyboard.current.spaceKey.isPressed) && grounded == true && currentCooldownTime <= 0)
         {
             rb.linearVelocityY += jumpHeight;
             currentCooldownTime = jumpCooldown;
-
+            
         }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.linearVelocityX = speed;
+            facingRight = true;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            rb.linearVelocityX = -speed;
+            facingRight = false;
+        }
+        
+        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 3)
+        contactPoint = collision.GetContact(0);
+        if(collision.gameObject.layer == 3 && contactPoint.normal == Vector2.up)
         {
             grounded = true;
             rb.linearVelocityY = 0f;
+         
         }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 3)
+        if (collision.gameObject.layer == 3 )
         {
             grounded = false;
         }
