3c19e1f08a563730da9612b0e37f9d3a853eebef
CanadianEagle11 <whcp2020@gmail.com>
2026-08-25 13:01:41 +0200

    SPIN spin virker tror jeg

--- cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/spinspin.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/spinspin.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class spinspin : GraftablePart
 {
-        float wait = 0.5f;
-        float time = 0;
-        float sub = -100;
-        bool a = true;
+    float wait = 0.5f;
+    float time = 0;
+    float sub = -100;
+    bool a = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
     protected override void OnAttach()
     {
@@ -20,30 +20,38 @@ public class spinspin : GraftablePart
             if (transform.position.y > sub)
             {
                 sub = transform.position.y;
-            } 
-           
+            }
+
         }
     }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+        PlayerMovement player = GetComponentInParent<PlayerMovement>();
+            if (player.canJump)
             {
-                time += Time.deltaTime;
+            sub = transform.position.y;
+            GetComponent<SpriteRenderer>().color = Color.white;
+            transform.GetComponentInParent<Rigidbody2D>().gravityScale = 1f;
+            }
+            time += Time.deltaTime;
                 if (time > wait)
                 {
                     time = 0;
                     if (sub > transform.position.y)
                     {
-                    sub = transform.position.y;
-                    GetComponent<SpriteRenderer>().color = Color.yellow;
-                    transform.GetComponentInParent<Rigidbody2D>().gravityScale=0.1f;
-                    }else if (sub <= transform.position.y) 
-                { 
-                    sub = transform.position.y;
-                    GetComponent<SpriteRenderer>().color = Color.white;
-                    transform.GetComponentInParent<Rigidbody2D>().gravityScale = 1f;
+                        sub = transform.position.y;
+                        GetComponent<SpriteRenderer>().color = Color.yellow;
+                        transform.GetComponentInParent<Rigidbody2D>().gravityScale = 0.1f;
+                    }
+                    else if (sub <= transform.position.y)
+                    {
+                        sub = transform.position.y;
+                        GetComponent<SpriteRenderer>().color = Color.white;
+                        transform.GetComponentInParent<Rigidbody2D>().gravityScale = 1f;
+                    }
                 }
-                }
-            }
+             
+        }
     }
-}
