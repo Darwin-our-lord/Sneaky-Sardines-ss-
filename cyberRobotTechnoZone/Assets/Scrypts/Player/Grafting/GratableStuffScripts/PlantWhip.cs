055a81b7508fe27a9a5b0acf96c285a06073bf75
Darwin-our-lord <David@prebensen.dk>
2026-08-31 13:39:43 +0200

    Revert "atack"
    This reverts commit 19845e532cdca896bf12e389a2f50a32f41b3071.

--- cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/PlantWhip.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/PlantWhip.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 
 public class PlantWhip : GraftablePart
 {
-
+    Animator animator;
+    [SerializeField] AudioClip attackSound; // the sound of the whip attack 
+    [SerializeField] AudioClip attackHitSound; // the sound of the whip hitting an enemy
+    protected override void OnAttach()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log("PlantWhip attached");
+    }
+    private void Update()
+    {
+        if (held && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Perform whip attack
+            animator.SetTrigger("Attack");
+            AudioSource.PlayClipAtPoint(attackSound, transform.position, 1f);
+        }
+    }
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!held && collision.transform.gameObject.CompareTag("Player"))
+        base.OnTriggerEnter2D(collision);
+
+        if (held && collision.gameObject.CompareTag("Enemy"))
         {
-            held = true;
-
-            LeafAttack.held = true;
-
-            Destroy(gameObject);
+            collision.GetComponent<Enemy>().TakeDamage(1);// Perform damage to the enemy
+            AudioSource.PlayClipAtPoint(attackHitSound, transform.position, 1f);
         }
     }
 }
