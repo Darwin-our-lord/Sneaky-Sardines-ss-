7fcfb034d84441f116a5394b8ff101482e62b764
The-seagull-man <mail@marenvalentin.dk>
2026-08-31 15:04:57 +0200

    jeg har added T LYDEN LYDER

--- cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/PlantWhip.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/Player/Grafting/GratableStuffScripts/PlantWhip.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlantWhip : GraftablePart
 {
+    [SerializeField] AudioClip AttachSound; // The sound of the player attaching the whip
+
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (!held && collision.transform.gameObject.CompareTag("Player"))
@@ -9,6 +11,8 @@ public class PlantWhip : GraftablePart
             held = true;
             transform.position = collision.transform.position;
             collision.gameObject.GetComponent<LeafAttack>().held = true; // Set the static variable to true when the whip is picked up
+            AudioSource.PlayClipAtPoint(AttachSound, transform.position, 10f);
+
             Destroy(this.gameObject);
         }
     }
