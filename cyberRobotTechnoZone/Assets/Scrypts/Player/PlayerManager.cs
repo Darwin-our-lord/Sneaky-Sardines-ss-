06310b25ffd51b920f7f78c6db783e3145012563
Darwin-our-lord <David@prebensen.dk>
2026-09-01 13:43:16 +0200

    hurt heads work

--- cyberRobotTechnoZone/Assets/Scrypts/Player/PlayerManager.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/Player/PlayerManager.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] List<Sprite> headsprite;
-    [SerializeField] GameObject playerHeadObj;
     [SerializeField] MenuManager menuManager;
     [SerializeField] Animator deathAnimator;
     [SerializeField] int health = 3;
     [SerializeField] private int maxHealth = 3;
+
+
     float invincibilityDuration = 0.5f; // Duration of invincibility in seconds
     float lastDmgTime = 0.0f;
 
     [SerializeField] AudioClip HurtSound; // The sound of the player getting hurt
+
+    [Header("Head Sprites")]
+    [SerializeField] List<Sprite> headSpritesNormal;
+    [SerializeField] List<Sprite> headSpritesUp;
+    [SerializeField] List<Sprite> headSpritesDown;
+    [SerializeField] GameObject upPlayerHeadObj;
+    [SerializeField] GameObject downPlayerHeadObj;
+    [SerializeField] GameObject normalPlayerHeadObj;
     public void Heal(int amount)
     {
         health += amount;
@@ -35,14 +43,28 @@ public class PlayerManager : MonoBehaviour
         health -= damage;
         AudioSource.PlayClipAtPoint(HurtSound, transform.position, 1000f);
         if (health <= 0) { StartCoroutine(Die()); }
-        if (playerHeadObj != null && headsprite[health] != null)
+
+
+        if (normalPlayerHeadObj != null && headSpritesNormal[health - 1] != null)
         {
-            playerHeadObj.GetComponent<SpriteRenderer>().sprite = headsprite[health];
+            normalPlayerHeadObj.GetComponent<SpriteRenderer>().sprite = headSpritesNormal[health-1];
+        }
+        if (upPlayerHeadObj != null && headSpritesUp[health - 1] != null)
+        {
+            upPlayerHeadObj.GetComponent<SpriteRenderer>().sprite = headSpritesUp[health-1];
+        }
+        if (downPlayerHeadObj != null && headSpritesDown[health - 1] != null)
+        {
+            downPlayerHeadObj.GetComponent<SpriteRenderer>().sprite = headSpritesDown[health-1];
         }
     }
 
     IEnumerator Die()
     {
+        normalPlayerHeadObj.SetActive(false);
+        upPlayerHeadObj.SetActive(false);
+        downPlayerHeadObj.SetActive(false);
+
         deathAnimator.SetTrigger("Death");
         yield return new WaitForSeconds(3f);
         Time.timeScale = 1f;
