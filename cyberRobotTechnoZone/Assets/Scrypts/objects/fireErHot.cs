2e138740b545fb5b1f0ad6370ca10a24befccabe
CanadianEagle11 <whcp2020@gmail.com>
2026-09-02 20:29:37 +0200

    super duper spoky

--- cyberRobotTechnoZone/Assets/Scrypts/objects/fireErHot.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/objects/fireErHot.cs
@@ -1,25 +1,23 @@
 using UnityEngine;
 
-public class walltriger : MonoBehaviour
+public class fireErHot : MonoBehaviour
 {
-    public static bool dragerErActiv = false;
+    bool awwww = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        dragerErActiv = false;
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            dragerErActiv = true;
-           Debug.Log("du burgt være skremt");
+            collision.GetComponent<PlayerManager>().TakeDamage(1);
         }
-
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
