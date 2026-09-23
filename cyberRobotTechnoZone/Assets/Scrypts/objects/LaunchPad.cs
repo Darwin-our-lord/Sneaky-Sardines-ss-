1387e1bdacddbc5566526fade45ba969883f7349
raxmux <raxmuxdk@gmail.com>
2026-09-23 13:04:24 +0200

    Update LaunchPad.cs

--- cyberRobotTechnoZone/Assets/Scrypts/objects/LaunchPad.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/objects/LaunchPad.cs
@@ -5,6 +5,7 @@ public class LaunchPad : MonoBehaviour
     public GameObject player;
     public float fortniteLaunchPadForce = 0f;
     private PlayerMovement playerMovement;
+    private Animator playerAnimator; 
     Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,11 +18,13 @@ public class LaunchPad : MonoBehaviour
     {
         rb.AddForceY(fortniteLaunchPadForce);
         playerMovement.grounded = false;
+        playerAnimator.Play("Jump", 0, 0f);
         // Animation.DestroyObject;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ManFlyver();
+
     }
     // Update is called once per frame
     void Update()
