c2836145047f5da3a201697946b02e395b5134ef
raxmux <raxmuxdk@gmail.com>
2026-09-23 13:30:50 +0200

    david hjælp

--- cyberRobotTechnoZone/Assets/Scrypts/objects/LaunchPad.cs
+++ cyberRobotTechnoZone/Assets/Scrypts/objects/LaunchPad.cs
@@ -5,30 +5,57 @@ public class LaunchPad : MonoBehaviour
     public GameObject player;
     public float fortniteLaunchPadForce = 0f;
     private PlayerMovement playerMovement;
-    private Animator playerAnimator; 
+    private Animator playerAnimator;
     Rigidbody2D rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool canLaunch = true;
+
     void Start()
     {
         player = GameObject.Find("Player 1");
         rb = player.GetComponent<Rigidbody2D>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+        playerAnimator = player.GetComponent<Animator>();
     }
+
     void ManFlyver()
     {
-        rb.AddForceY(fortniteLaunchPadForce);
+        canLaunch = false;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);
+        rb.AddForceY(fortniteLaunchPadForce, ForceMode2D.Impulse);
+
         playerMovement.grounded = false;
+        playerMovement.enabled = false;
+
+        playerAnimator.SetFloat("VerticalVelocity", 100f);
         playerAnimator.Play("Jump", 0, 0f);
-        // Animation.DestroyObject;
+
+        Invoke(nameof(ResetScript), 0.2f);
+        Invoke(nameof(ResetCooldown), 0.5f);
     }
+
+    void ResetScript()
+    {
+        playerAnimator.SetFloat("VerticalVelocity", 100f);
+        playerMovement.enabled = true;
+    }
+
+    void ResetCooldown()
+    {
+        canLaunch = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        ManFlyver();
-
+        if (canLaunch && collision.gameObject == player)
+        {
+            ManFlyver();
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        
+
     }
 }
